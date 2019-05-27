f056fe28a4b2d0b3037140ca427a28d9db6a5cfc
3Mydlo3 <mydlo333@interia.pl>
2019-05-27 18:07:32 +0200

    [Main] - Create showcase script

--- main.py
+++ main.py
@@ -3,5 +3,26 @@ from blob import Blob
 from food import Food
 from world import World
 
+"""
+    SCRIPT
+"""
 
-pass
+# Create squared world 25x25
+ddd = World(25)
+# Randomly generate 10 food
+ddd.generateFood(10)
+# Show map
+ddd.draw()
+print("Map with Food: ")
+print(ddd.area)
+# Food is yellow
+
+# Create Blob
+u = Blob()
+# Position Blob at 4,4
+ddd.area[4,4] = u
+# Show map
+ddd.draw()
+print("Map with Food and Blob: ")
+print(ddd.area)
+# Blob is now yellow, and food is green
