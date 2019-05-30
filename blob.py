07104cde16c06a1ee01a212db13e0150e0fa5086
3Mydlo3 <mydlo333@interia.pl>
2019-05-30 07:57:06 +0200

    [Blob] Add position shift calculation after movement

--- blob.py
+++ blob.py
@@ -1,3 +1,13 @@
+from random import random
+from math import radians, cos, sin
+
 class Blob:
     def __init__(self):
-        pass
+        self.relativePosition = (0, 0)
+        self.speed = 1
+
+    def move(self):
+        direction = random()*360
+        dx = cos(radians(direction)) * self.speed
+        dy = sin(radians(direction)) * self.speed
+        self.relativePosition = (self.relativePosition[0] + dx, self.relativePosition[1] + dy)
