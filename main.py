2fe67233de83e6a0ad7dcacb78f2a3c7264f9069
3Mydlo3 <mydlo333@interia.pl>
2019-05-31 12:39:50 +0200

    [Main] Add blobs and movement to showcase script

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from world import World
 """
 
 # How many days to run simulation for
-simulationDuration = 3
+simulationDuration = 15
 # Number of new food generated each day
 foodQuantity = 10
 # World size (square)
@@ -16,10 +16,15 @@ worldSize = 25
 
 # Initialize world
 world = World(worldSize)
-for i in range(0, 30):
-    blob1 = Blob(world)
+blobs = []
+for i in range(0, 10):
+    blobs.append(Blob(world))
 # Run simulation
 for dayNumber in range (0, simulationDuration):
     world.generateFood(foodQuantity)
+    # Show map at the day start
+    world.draw()
+    for blob in blobs:
+        blob.move(world)
     # Show map at the day end
     world.draw()
